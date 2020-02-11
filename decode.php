9687f01be2ccec8367b21ba0bdc8aec6e2a86224
andrie1240 <55270566+andrie1240@users.noreply.github.com>
2020-02-11 19:52:06 +0800

    Add files via upload

---
+++ decode.php
@@ -0,0 +1,25 @@
+<?php
+// header('Content-Type: application/json');
+
+$db_username = 'admin';
+$db_password = 'root1234';
+$db_hostname = 'dbrojasdev.cjw42bnplsor.us-east-1.rds.amazonaws.com';
+$db_port = '3306';
+$db_name = 'db_1822188';
+
+$conn = mysqli_connect($db_hostname, $db_username, $db_password, $db_name);
+$q = "SELECT * FROM table2";
+$r = mysqli_query($conn, $q);
+
+while ($item = mysqli_fetch_object($r)) {
+    $json = json_encode($item);
+	$dec = json_decode($json, true);
+	
+	echo "<ul>";
+	echo "<li>". $dec["id"] ."</li>";
+	echo "<ul>";
+	echo "<li>". $dec["username"] ."</li>";
+	echo "<li>". $dec["password"] ."</li>";
+	echo "</ul>";
+	echo "</ul>";
+}
